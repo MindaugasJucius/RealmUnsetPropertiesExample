fb08306784b3c41ad505d958fcfd8784b4df41b8
Mindaugas Jucius <mindaugas.jucius@icloud.com>
2017-12-12 16:05:23 +0200

    Initial Commit

---
+++ RealmUnsetPropertiesExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmUnsetPropertiesExample
+//
+//  Created by Mindaugas Jucius on 12/12/2017.
+//  Copyright © 2017 Mindaugas Jucius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
